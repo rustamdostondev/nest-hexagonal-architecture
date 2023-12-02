7ce12b97f74d20dbdf1e85d838d636622f679525
dostonbekoktamov <rustamdostondev@gmail.com>
2023-12-02 21:33:42 +0500

    add: filet exception finished

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as path from 'path';
+import { AllExceptionFilter } from './infrastructure/common/filter/exception.filter';
+import { LoggerService } from './infrastructure/logger/logger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const PORT = 3000;
+
+  // Filter
+  app.useGlobalFilters(new AllExceptionFilter(new LoggerService()));
+
   await app.listen(PORT, () => {
     console.log('Server url : http://localhost:' + PORT);
   });

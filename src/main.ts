0d090d8d272daef46d6e8eb9a8fe0b2df4451289
dostonbekoktamov <rustamdostondev@gmail.com>
2023-12-02 17:58:56 +0500

    moodule and config added

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as path from 'path';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const PORT = 3000;
+  await app.listen(PORT, () => {
+    console.log('Server url : http://localhost:' + PORT);
+  });
+}
+bootstrap();
